5fcc9eafafdcaf6cfa1f5bd02986194f9211f4e2
Manoj Mukherjee <Manoj.Mukherjee@optiv.com>
2019-09-24 14:27:37 +0530

    Updates

---
+++ precache-manifest.a6370eb38ddf8f1b23df35d9be512b2a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32e9425c1dffa87fd0c94e17f329278",
+    "url": "/weddinginvitation/index.html"
+  },
+  {
+    "revision": "5d0f18bd33b3191f8204",
+    "url": "/weddinginvitation/static/css/main.3ad28e37.chunk.css"
+  },
+  {
+    "revision": "1c06872badd8ea0f3314",
+    "url": "/weddinginvitation/static/js/2.96fdda44.chunk.js"
+  },
+  {
+    "revision": "5d0f18bd33b3191f8204",
+    "url": "/weddinginvitation/static/js/main.69029ff4.chunk.js"
+  },
+  {
+    "revision": "64881349ae386a4f76a1",
+    "url": "/weddinginvitation/static/js/runtime~main.28db7b44.js"
+  },
+  {
+    "revision": "4fecfaadb34f86867d0c7ae5c25e315f",
+    "url": "/weddinginvitation/static/media/Background_Circle.4fecfaad.png"
+  },
+  {
+    "revision": "9b14f9e6765b6de220768a0df0fc9109",
+    "url": "/weddinginvitation/static/media/a123.9b14f9e6.png"
+  },
+  {
+    "revision": "7f5bc7f3913b72e421983e869024afed",
+    "url": "/weddinginvitation/static/media/ap.7f5bc7f3.png"
+  },
+  {
+    "revision": "155af7fd1e56340dc2cec7bd2761e203",
+    "url": "/weddinginvitation/static/media/brideback.155af7fd.jpg"
+  },
+  {
+    "revision": "0848d9d0f415ea63d3ecce98e586b461",
+    "url": "/weddinginvitation/static/media/counter-1-1600x600.0848d9d0.jpg"
+  },
+  {
+    "revision": "82c8fbe284a653442e684c21bbe4336c",
+    "url": "/weddinginvitation/static/media/logo-white.82c8fbe2.png"
+  },
+  {
+    "revision": "884a06cb027b4caf78dcba4959b58c34",
+    "url": "/weddinginvitation/static/media/mana2.884a06cb.jpg"
+  },
+  {
+    "revision": "dc439258993066c26ea3761ce1a3c3e5",
+    "url": "/weddinginvitation/static/media/manoj1.dc439258.png"
+  },
+  {
+    "revision": "0403d1502015855d1aaaab005caff9c7",
+    "url": "/weddinginvitation/static/media/met3.0403d150.jpg"
+  },
+  {
+    "revision": "99e3843bf880c87d204db13cd11f7ff4",
+    "url": "/weddinginvitation/static/media/met4.99e3843b.jpg"
+  },
+  {
+    "revision": "7c921ddc6f8690e43063bac6ea6d4341",
+    "url": "/weddinginvitation/static/media/met5.7c921ddc.jpg"
+  },
+  {
+    "revision": "bf69246006360e41647ecae601e247fa",
+    "url": "/weddinginvitation/static/media/slider-1-1600x900.bf692460.jpg"
+  },
+  {
+    "revision": "19ae42dbe0f8ac04f58993f4e70425a6",
+    "url": "/weddinginvitation/static/media/w22.19ae42db.png"
+  },
+  {
+    "revision": "1f0312b65ec66e7a3ade40aecf48a6af",
+    "url": "/weddinginvitation/static/media/webzed-font-1.1f0312b6.woff"
+  },
+  {
+    "revision": "6ed2107f2da89dcf9e52f006e878b679",
+    "url": "/weddinginvitation/static/media/webzed-font-1.6ed2107f.ttf"
+  },
+  {
+    "revision": "d5939ecfb23c9b4504264e0c4234ba30",
+    "url": "/weddinginvitation/static/media/webzed-font-1.d5939ecf.eot"
+  },
+  {
+    "revision": "e65681d7a99427774c50261520216889",
+    "url": "/weddinginvitation/static/media/webzed-font-1.e65681d7.svg"
+  }
+]);
